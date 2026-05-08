3d65542c7673c78cddc1fbcf107b8a12e298ab76
Marcio Antônio de Andrade Bortoloti <mbortoloti@uesb.edu.br>
2026-05-08 09:39:50 -0300

    Update example 6.2.2

--- Example_6.2.2/example622.jl
+++ Example_6.2.2/example622.jl
@@ -25,21 +25,17 @@ n = 50
 # Identity matrix
 In = Matrix{Float64}(la.I,n,n)
 
-# Function definitions
+# Objective function definitions
 α = 0.5
 g1(_,X) = α * la.tr(X)
 grad_g1(_,X) = α * In
-
 μ = 1.e-3
 A = la.Diagonal([i for i in 1:n])
 g2(_,X) = la.tr(inv(X)*A)+la.logdet(X) - n
 grad_g2(_,X) = -inv(X) * (A * inv(X) - In)
-    
 B =  μ * A
 h(_,X) = la.tr(B * X)
 ∂h(_,X) = B
-
-
 
 f(M,X) = g1(M,X) + g2(M,X) - h(M,X)
 
@@ -61,6 +57,7 @@ f_X0 = f(M,X0)
 # Set maximum of iterations
 maxiter = 1000
             
+# λ0 analysis 
 
 λ0 = λ[1]
 S = adap_rppm(M,X0,g1,grad_g1,g2,grad_g2,h,∂h,λ0,maxiter,ϵ;
@@ -87,22 +84,23 @@ S = adap_rppm(M,X0,g1,grad_g1,g2,grad_g2,h,∂h,λ0,maxiter,ϵ;
               return_state=true);
     push!(cost_adap,S)
 
-# Ploting  
-plot([],[],
-     label="",yticks=([1.e-14,1.e-12,1.e-10,1.e-8,1.e-6,1.e-4,1.e-2,1.e+0]),
-     ylabel=L"\log\|f(x^k)-f(x^*)\|",
-     xlabel="Iteration",
-     guidefont= font(12),
-     tickfont = font(12),
-     legendfont = font(12));
-
-
-# Set critical point
+# Set critical point for the function above
 function xs(i)
     mi = α - μ * i
     return (-1 + sqrt(1 + 4 * mi * i))/(2mi)
 end
 Xs = la.Diagonal([xs(i) for i in 1:n])
+
+
+# Ploting  λ0 analysis
+plot([],[],
+     label="",yticks=([1.e-14,1.e-12,1.e-10,1.e-8,1.e-6,1.e-4,1.e-2,1.e+0]),
+     ylabel=L"\log\|f_2(x^k)-f_2(x^*)\|",
+     xlabel="Iteration",
+     guidefont= font(12),
+     tickfont = font(12),
+     legendfont = font(12));
+
 
 # Calculate f(Xs)
 f_Xs = f(M,Xs)
