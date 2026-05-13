6b5f5343043b41cbe4c1f25a4d96877e8bcd686c
Marcio Antônio de Andrade Bortoloti <mbortoloti@uesb.edu.br>
2026-05-13 09:13:56 -0300

    Update example621.jl

--- Example_6.2.1/example621.jl
+++ Example_6.2.1/example621.jl
@@ -11,11 +11,11 @@ include("../solvers/adap_rppm_sgm.jl")
 seed = MersenneTwister(1234)
 
 # dimensões
-n = 50         # dimensão da esfera
-p = 10        # número de colunas
-m = 20          # linhas de A
+n = 50         
+p = 10        
+m = 20        
 
-# dados
+# Set objective function 
 A = randn(seed,m, n)
 
 A .-= mean(A,dims=1)
@@ -30,27 +30,25 @@ initx = PCAV
 tmp = A * PCAV
 maxvar = sum(tmp.^2)
 
-#D = Diagonal(S[1:p])
 
-#Dsq = D.^2
 Dsq = S[1:p].^2
 
-#lambda = 1 
 
-#Dsq = rand(p)
 λ = 1.e-1;
 L = norm(Dsq)^2;
 
 tol = 1e-8
 maxiter = 10000
 
-# ponto inicial na variedade oblíqua
+# Initial guess on manifold
 M = Manifolds.PowerManifold(Sphere(n - 1), p)
 X0 = rand(seed,M)
 
-# rodar
+# RPG solver
 Xopt, iter, time, fv, err, sparsity, avar, fs =
     Driver_MPGWH(X0, A, Dsq, λ, L, tol, maxiter);
+
+#Set objective function for Adap-GR-PPM
 
 D2 = Diagonal(Dsq)
 
@@ -69,7 +67,11 @@ grad_g2(_,X) = 4.0 * AtA * X * X' * AtA * X
 grad_h(_,X) = 4.0*AtA*X*D2
 
 M2 = Manifolds.Oblique(n,p)
+
+# Set initial λ0
 λ0 = 100.0;
+
+# Adap-GR-PPM solver
 S = adap_rppm(M2,X0,g1,grad_g1,g2,grad_g2,h,grad_h,λ0,maxiter,tol);
 
 
