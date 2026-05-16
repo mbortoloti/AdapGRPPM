2df2d03a5bef9b72a353777f91a278cca08a2302
Marcio Antônio de Andrade Bortoloti <mbortoloti@uesb.edu.br>
2026-05-16 08:31:23 -0300

    Update adap_grppm_sgm.jl

--- solvers/adap_grppm_sgm.jl
+++ solvers/adap_grppm_sgm.jl
@@ -49,8 +49,6 @@ end
 function build_grad_fk(M :: mf.AbstractManifold, Y :: Matrix{Float64}, Zk :: Matrix{Float64}, 
         grad_g1 :: Function, λk :: Float64)
 
-   # r_grad_g1_Y = Y * grad_g1(M,Y) * Y
-    #r_grad_g1_Y = md.riemannian_gradient(M,Y,grad_g1(M,Y))
 
     r_grad_g1_Y = md.project(M,Y,grad_g1(M,Y))
 
@@ -68,6 +66,7 @@ function adap_grppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Funct
     
     # Set initial guess
     Xk = copy(M,X0)
+
     println("iter: ",0,"  ","f: ",f(M,Xk))
     f_Xk = f(M,Xk)
   
@@ -80,11 +79,9 @@ function adap_grppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Funct
     for iter in 1:maxiter
 
         # Calculate riemannian gradient of g2
-        #Vk = md.riemannian_gradient(M,Xk,grad_g2(M,Xk))
         Vk = mf.project(M,Xk,grad_g2(M,Xk))
 
         # Calculate riemannian subgradient of h
-        #Wk = md.riemannian_gradient(M,Xk,grad_h(M,Xk))
         Wk = mf.project(M,Xk,grad_h(M,Xk))
 
         WkmVk = Wk-Vk 
@@ -102,13 +99,7 @@ function adap_grppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Funct
 
             grad_gk(M,Y) = build_grad_gk(M,Y,Zk,λk)
             try
-                # Solving subproblem
-                #Yk = Manopt.trust_regions(M, fk, grad_fk, Zk;
-                     #stepsize=ArmijoLinesearch(M;contraction_factor=0.5,initial_stepsize=1.0,
-                     #stop_when_stepsize_less=1.e-16),
-                     #debug=[:Iteration, :Cost, "\n",:Stop],
-                #     stopping_criterion=StopAfterIteration(5000)|StopWhenChangeLess(M,1e-12)
-                #    );
+   
                 stepsize_opt = Manopt.DecreasingStepsize(M::AbstractManifold;
                                length=min(injectivity_radius(M)/2, 1.0),
                                factor=0.9,
@@ -118,44 +109,15 @@ function adap_grppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Funct
                                type=:relative
                                )
                 Yk = Manopt.subgradient_method(M, fk, grad_fk, Zk;
-                #stepsize = Manopt.DecreasingStepsize(M),
                 stepsize = stepsize_opt,
                 retraction_method = ProjectionRetraction(),
                 stopping_criterion=Manopt.StopAfterIteration(50)
                 )
-                #length=min(injectivity_radius(M)/2, 1.0),
-                #factor=1.5,
-                #subtrahend=0.0,
-                #exponent=0.5,
-                #shift=0.0,
-                #type=:relative)
- 
-                #Yk = Manopt.subgradient_method(M,fk,grad_fk,Xk;
-                #    #stepsize = DecreasingStepsize(0.5, 1, 0.5), # (comprimento inicial, expoente, etc)
-                #    stopping_criterion = StopAfterIteration(500),
-                #    return_state = true)
-
-                #stepsize = Manopt.DecreasingStepsize(M),
-                #stepsize = ArmijoLinesearch(M;contraction_factor=0.5,initial_stepsize=1.0,stop_when_stepsize_less=1.e-16),
-                #debug = [:Iteration, :Cost],
-                #stopping_criterion = Manopt.StopAfterIteration(50000))
-
-                #Yk = Manopt.proximal_gradient_method(M, fk, gk, grad_gk,Xk)
-                
-                #Yk = Manopt.convex_bundle_method(M, fk, grad_fk, Xk);
 
                 # Only for avoid numerical issues
                 Yk = Manifolds.project(M,Yk)
 
-                #debug=[:Iteration, :Cost, "\n",:Stop])
                 
-
-                #Yk = Manopt.proximal_gradient_method(M,fk,gk,grad_gk,Xk;debug=[:Iteration, :Cost, "\n",:Stop])
-                #;
-                #        stepsize = 1.0,stopping_criterion = StopWhenGradientNormLess(1e-6) | StopAfterIteration(5000),
-                #        debug = []
-                #        )
-
                 if ~(Manifolds.is_point(M,Yk))
                     println("Yk is not on manifold!")
                 end
@@ -163,10 +125,6 @@ function adap_grppm(M :: mf.AbstractManifold, X0 :: Matrix{Float64}, g1 :: Funct
                 global dist_Xk_Yk = mf.distance(M,Xk,Yk)
 
                 f_Yk = f(M,Yk)
-                #push!(cost,f_Yk)
-                # Teste
-                #η = Manifolds.project(M,Xk,Yk-Xk)
-                #println("norm η = $(norm(η,2)^2)")
 
                 if dist_Xk_Yk < ϵ
                     
